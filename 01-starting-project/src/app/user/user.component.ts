f6c1ea5a29a5a9c9b3f806fcf5c0c38c0d6e16d8
Awasthi <archit-kumar.awasthi@capgemini.com>
2025-05-16 15:27:19 +0530

    adding remaining changes

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length) 
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length) 
 
 @Component({
   selector: 'app-user',
@@ -11,5 +11,15 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  
+  @Input() avatar!: string;
+  @Input() name!: string;
+  @Input() id!: string;
+
+  get imagePath() {
+    return "assets/users/" + this.avatar;
+  }
+
+  onSelectUser() {}
+
 }
